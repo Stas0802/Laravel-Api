981637ed2b6150e9bb7746c3646367e719878572
Stas0802 <stasshulga6@gamail.com>
2025-09-12 11:12:12 +0300

    add rote

--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,3 +14,6 @@ Route::post('/register',[AuthController::class, 'register']);
 Route::post('/login',[AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
+Route::middleware('auth:sanctum')->apiResource('/categories', CategoryController::class);
+
+Route::apiResource('/products', ProductController::class);
