e860f4f90275b6be37db286c32854abba3b0a113
Stas0802 <stasshulga6@gamail.com>
2025-09-12 11:14:20 +0300

    add model Product

---
+++ laravel-api/app/Models/Product.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name',
+        'image',
+        'description',
+        'price',
+        'category_id',
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
+
