f3c25f29d6f16cbcb782d166428572a8cfd03dd7
Stas0802 <stasshulga6@gamail.com>
2025-09-12 11:13:40 +0300

    add  CategoryController

---
+++ laravel-api/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CategoryRequest;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(Category::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CategoryRequest $request)
+    {
+        $category = Category::create($request->validated());
+        return response()->json($category, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category)
+    {
+        return response()->json($category);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(CategoryRequest $request, Category $category)
+    {
+        $category->update($request->validated());
+        return response()->json($category);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        if($category->products()->count() > 0){
+            return response()->json(['message' => 'Cannot delete category with product'], 400);
+        }
+        $category->delete();
+        return response()->json(['message' => 'Category delete successfully']);
+    }
+}
