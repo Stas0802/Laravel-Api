4a8f33dce72bd51400a0e3398a619d8255ee0ccf
Stas0802 <stasshulga6@gamail.com>
2025-09-12 16:25:47 +0300

    add Route on comment

--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,10 +11,18 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+/**
+ * Route on work with users
+ */
 Route::post('/register',[AuthController::class, 'register']);
 Route::post('/login',[AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
+/**
+ * Route on work with products
+ */
 Route::middleware('auth:sanctum')->apiResource('/categories', CategoryController::class);
 
 Route::apiResource('/products', ProductController::class);
+
+Route::apiResource('/products.comments', CommentController::class)->middleware('auth:sanctum');
